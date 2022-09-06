986fe06751c64f45db7ad0ea460892d70e49ab0e
hye0e <nanda5050@naver.com>
2022-09-06 11:04:53 +0900

    인증 API - ExceptionTranslationFilter

--- src/main/java/io/security/basicsecurity/SecurityConfig.java
+++ src/main/java/io/security/basicsecurity/SecurityConfig.java
@@ -1,12 +1,25 @@
 package io.security.basicsecurity;
 
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.security.web.access.AccessDeniedHandler;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import org.springframework.security.web.savedrequest.HttpSessionRequestCache;
+import org.springframework.security.web.savedrequest.RequestCache;
+import org.springframework.security.web.savedrequest.SavedRequest;
 
 @Configuration
 @EnableWebSecurity
@@ -17,8 +30,6 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter { // WebSecurit
 
     /**
      * 사용자 생성
-     * @param auth
-     * @throws Exception
      */
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
@@ -31,12 +42,23 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter { // WebSecurit
     protected void configure(HttpSecurity http) throws Exception {
         http
             .authorizeRequests()
+            .antMatchers("/login").permitAll()
             .antMatchers("/user").hasRole("USER") // 지정한 요청이 온다면 권한 심사를 하겠다.
             .antMatchers("/admin/pay").hasRole("SYS")
             .antMatchers("/admin/**").access("hasRole('ADMIN') or hasRole('SYS')")
             .anyRequest().authenticated(); // 모든 요청에 대해 보안요청을 하겠다.
         http
-            .formLogin(); // 폼 로그인 인증방식 작동
+            .formLogin() // 폼 로그인 인증방식 작동
+            .successHandler(new AuthenticationSuccessHandler() {
+                @Override
+                public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response,
+                    Authentication authentication) throws IOException, ServletException {
+                    RequestCache requestCache = new HttpSessionRequestCache();
+                    SavedRequest savedRequest = requestCache.getRequest(request, response); // 원래 사용자가 가지고 있던 정보들을 가지고 있음
+                    String redirectUrl = savedRequest.getRedirectUrl(); // 사용자가 원래 가려고 했던 url을 가져올 수 있음
+                    response.sendRedirect(redirectUrl);
+                }
+            });
 //            .loginPage("/loginPage") // 로그인 페이지 설정
 //            .defaultSuccessUrl("/") // 로그인 성공 시 리다이렉트 시킬 url
 //            .failureUrl("/login_proc") // 로그인 실패 시 리다이렉트 시킬 url
@@ -85,11 +107,26 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter { // WebSecurit
 //            .userDetailsService(userDetailsService) // user 정보 조회하는 기능처리
 //        ;
 
-
 //        http
 //            .sessionManagement()
 //            .maximumSessions(1)
 //            .maxSessionsPreventsLogin(true);
+
+        http.exceptionHandling()
+            .authenticationEntryPoint(new AuthenticationEntryPoint() {
+                @Override
+                public void commence(HttpServletRequest request, HttpServletResponse response,
+                    AuthenticationException authException) throws IOException, ServletException {
+                    response.sendRedirect("/login");
+                }
+            })
+            .accessDeniedHandler(new AccessDeniedHandler() {
+                @Override
+                public void handle(HttpServletRequest request, HttpServletResponse response,
+                    AccessDeniedException accessDeniedException) throws IOException, ServletException {
+                    response.sendRedirect("/denied");
+                }
+            });
 
 
     }
--- 인증API-ExceptionTranslationFilter.md
+++ 인증API-ExceptionTranslationFilter.md
@@ -8,3 +8,48 @@
       - SavedRequest - 사용자가 요청했던 request 파라미터 값들, 그 당시의 헤더값들 등이 저장
 - AccessDeniedException
   - 인가 예외 처리
+  - AccessDeniedHandler에서 예외 처리하도록 제공
+
+- http.exceptionHandling(): 예외처리 기능이 작동함
+
+```java
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter { // WebSecurityConfigurerAdapter deprecated
+
+  @Autowired
+  UserDetailsService userDetailsService;
+
+  @Override
+  protected void configure(HttpSecurity http) throws Exception {
+    http
+            .formLogin() // 폼 로그인 인증방식 작동
+            .successHandler(new AuthenticationSuccessHandler() {
+              @Override
+              public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response,
+                      Authentication authentication) throws IOException, ServletException {
+                RequestCache requestCache = new HttpSessionRequestCache();
+                SavedRequest savedRequest = requestCache.getRequest(request, response); // 원래 사용자가 가지고 있던 정보들을 가지고 있음
+                String redirectUrl = savedRequest.getRedirectUrl(); // 사용자가 원래 가려고 했던 url을 가져올 수 있음
+                response.sendRedirect(redirectUrl);
+              }
+            });
+
+    http.exceptionHandling()
+            .authenticationEntryPoint(new AuthenticationEntryPoint() { // 인증 실패 시 처리
+              @Override
+              public void commence(HttpServletRequest request, HttpServletResponse response,
+                      AuthenticationException authException) throws IOException, ServletException {
+                response.sendRedirect("/login"); // 스프링 시큐리티가 제공하는 로그인 페이지가 아님 
+              }
+            })
+            .accessDeniedHandler(new AccessDeniedHandler() { // 인증 실패 시 처리
+              @Override
+              public void handle(HttpServletRequest request, HttpServletResponse response,
+                      AccessDeniedException accessDeniedException) throws IOException, ServletException {
+                response.sendRedirect("/denied");
+              }
+            });
+  }
+}
+```
